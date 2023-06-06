1cb611ea0a72607e95e9493062ad64aa5931b6ca
Charry Wu <charry@evt.ai>
2023-06-06 11:30:37 -0700

    fix: sqlalchemy models error

--- python/db/models.py
+++ python/db/models.py
@@ -10,8 +10,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
-    
-
 class File(Base):
     __tablename__ = "files"
 
@@ -19,7 +17,6 @@ class File(Base):
     url = Column(String, unique=True, index=True) # url to file, could be linux file schema, or http schema
     hash = Column(String, unique=True) # md5 hash of file, used for checking duplication
     is_active = Column(Boolean, default=True) # if inactive, file is not accessible/visible
-    
 
 
 class Job(Base):
@@ -32,19 +29,16 @@ class Job(Base):
     id = Column(Integer, primary_key=True, index=True) # Self-incrementing ID of job
     status = Column() # UNASSIGNED = 0, ASSIGNED_PENDING = 1, PROCESSING = 2, COMPLETED = 3, FAIL = 4
     input_file_id = Column(Integer, ForeignKey("files.id"))
-    input_file = relationship(File, back_populates="jobs")
     output_file_id = Column(Integer, ForeignKey("files.id"))
-    output_file = relationship(File, back_populates="jobs")
     frame_rate = Column(String) # Frame rate of video, stored as string to allow fractional frame rate (“25/1” and “25” represent the same frame rate)
     num_frames = Column(Integer) # Total number of frames of video
     duration = Column(Float) # Duration of video, in seconds
-    user_id = Column(Integer, ForeignKey("files.id"))
-    uid = relationship("User", back_populates="jobs") # Foreign Key to the user table. The user id who created the job. For MVP, all jobs have uid=1.
-    wid = relationship("Worker", back_populates="jobs") # Foreign Key to the assigned worker id. Null if job is pending assignment
+    user_id = Column(Integer, ForeignKey("users.id")) # Foreign Key to the user table. The user id who created the job. For MVP, all jobs have uid=1.
+    worker_id = Column(Integer, ForeignKey("workers.id")) # Foreign Key to the assigned worker id. Null if job is pending assignment
     created_time = Column(DateTime) # Time when this job gets created
     process_start_time = Column(DateTime) # Time when a worker machine starts processing this job
     process_end_time = Column(DateTime) # Time when a worker machine fully processed this job
-    
+
 
 class QueueJob(Base):
     """
@@ -67,14 +61,14 @@ class Worker(Base):
     __tablename__ = "workers"
 
     id = Column(Integer, primary_key=True, index=True) # Self-incrementing worker machine
-    status = Column() # Idle=0, Running=1, Fail=2
+    status = Column() # Idle=0, Running=1, Fail=2, Complete=3
     worker_ip = Column(String)
     masters_ip = Column(String)
-    cur_job_id = relationship("Job", back_populates="queuejobs")
+    output_file_id = Column(Integer, ForeignKey("jobs.id"))
     started_time = Column(DateTime)
     heartbeat_time = Column(DateTime)
     # Message content of last heartbeat, in string JSON format.
     # Worker machine can use the heartbeat message to report any anomalies in machine conditions or job status. Useful for debug & analytics report
     heartbeat_msg = Column(String)
-   
+
 
